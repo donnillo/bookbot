e336a21f2f1f2523073bc89cd398160e56195f34
Kiselev Danila <kiselevda3@it.mos.ru>
2025-06-29 22:54:09 +0300

    fix my stupidity

--- paths.py
+++ paths.py
@@ -10,7 +10,7 @@ BOOKS = (SRC / "books").relative_to(SRC)
 
 def get_book_from_args() -> Path | None:
     if len(sys.argv) == 2:
-        path = pathlib.Path(sys.argv[0])
+        path = pathlib.Path(sys.argv[1])
         if path.parent == BOOKS:
             return path.name
         else:
