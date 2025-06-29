dc0acf4d30dc9d8fe032dc279ef6fd5b7c150d5a
Kiselev Danila <kiselevda3@it.mos.ru>
2025-06-29 22:43:21 +0300

    add simpliest argparser

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from reporter import Report
 from stats import word_count
 from stats import character_count
 from paths import BOOKS
+from paths import get_book_from_args
 
 
 def main():
-    with Report(BOOKS / "frankenstein.txt").set_width(30) as report:
-        print(f"Analyzing book found at {report.book}")
+    if (BOOK := get_book_from_args()) is not None:
+        with Report(BOOKS / "frankenstein.txt").set_width(30) as report:
+            print(f"Analyzing book found at {report.book}")
 
-        with report.stat(word_count) as num_of_words:
-            print(f"Found {num_of_words} total words")
+            with report.stat(word_count) as num_of_words:
+                print(f"Found {num_of_words} total words")
 
-        with report.stat(character_count, sort=True, top=10) as char_counts:
-            for char, count in char_counts.items():
-                print(f"{char}: {count}")
+            with report.stat(character_count, sort=True, top=10) as char_counts:
+                for char, count in char_counts.items():
+                    print(f"{char}: {count}")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
 
 
 main()
--- paths.py
+++ paths.py
@@ -1,3 +1,4 @@
+import sys
 import pathlib
 
 type Path = str | pathlib.Path
@@ -5,3 +6,12 @@ type Path = str | pathlib.Path
 SRC = pathlib.Path(__file__).parent
 APP_NAME = SRC.stem
 BOOKS = (SRC / "books").relative_to(SRC)
+
+
+def get_book_from_args() -> Path | None:
+    if len(sys.argv) == 2:
+        path = pathlib.Path(sys.argv[0])
+        if path.parent == BOOKS:
+            return path.name
+        else:
+            return path
